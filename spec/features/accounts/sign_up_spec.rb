d699c1ae50c554e4a6c458a39112a30792e26a31
Alex Rincon <alejandro.rincon9@gmail.com>
2017-07-27 23:21:12 -0500

    Link Accounts to Owners

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -5,7 +5,11 @@ feature "Accounts" do
     visit root_path
     click_link "Create a new account"
     fill_in "Name", with: "test"
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "password", exact: true
+    fill_in "Password confirmation", with: "password"
     click_button "Create Account"
+    expect(page).to have_content("Signed in as test@example.com")
 
     within(".flash_notice") do
       success_message = "Your account has been created."
