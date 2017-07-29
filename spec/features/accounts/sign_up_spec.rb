8c8532f09e5d94bfd8ed53ba60b75e85ad5c9d74
Alex Rincon <alejandro.rincon9@gmail.com>
2017-07-28 22:25:25 -0500

    Add uniqueness to subdomains

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -18,4 +18,22 @@ feature "Accounts" do
       expect(page).to have_content(success_message)
     end
   end
+
+  scenario "Ensure domain uniqueness" do
+    Account.create!(name: "Test", subdomain: "test")
+
+    visit root_path
+    click_link "Create a new account"
+    fill_in "Name", with: "Test"
+    fill_in "Subdomain", with: "test"
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "password", exact: true
+    fill_in "Password confirmation", with: "password"
+    click_button "Create Account"
+
+    expect(page.current_url).to eq("http://lvh.me/accounts")
+    expect(page).to have_content("Sorry, your account could not be created.")
+    subdomain_error = find(".account_subdomain .help-block").text
+    expect(subdomain_error).to eq("has already been taken")
+  end
 end
