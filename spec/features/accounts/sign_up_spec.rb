7bbd4fa5e360f97fde2b6cdf34dea7a8abd80df2
Alex Rincon <alejandro.rincon9@gmail.com>
2017-07-28 21:49:54 -0500

    Add subdomains to accounts

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -4,12 +4,14 @@ feature "Accounts" do
   scenario "creating an account" do
     visit root_path
     click_link "Create a new account"
-    fill_in "Name", with: "test"
+    fill_in "Name", with: "Test"
+    fill_in "Subdomain", with: "test"
     fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password", exact: true
     fill_in "Password confirmation", with: "password"
     click_button "Create Account"
     expect(page).to have_content("Signed in as test@example.com")
+    expect(page.current_url).to eq("http://test.lvh.me/")
 
     within(".flash_notice") do
       success_message = "Your account has been created."
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ Sidekiq::Testing.inline!
 
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
+Capybara.app_host = "http://lvh.me" 
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
